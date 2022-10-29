bb3f2e8e09523407d67f1570f56c9f831452a497
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-29 17:40:18 -0300

    Projeto inicializado e add as dependencias

---
+++ cryptoApp/src/main/java/com/reis/cryptoApp/CryptoAppApplication.java
@@ -0,0 +1,13 @@
+package com.reis.cryptoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CryptoAppApplication {
+
+	public static void main(String[] args) { SpringApplication.run(CryptoAppApplication.class, args);}
+	
+}
---
+++ cryptoApp/src/test/java/com/reis/cryptoApp/CryptoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reis.cryptoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
